943416c0d010f8af26dfc6f97380f6b317d0a021
elterror6 <danibasketball2000@gmail.com>
2025-06-05 12:58:39 +0200

    add(logger): Adición de un logger para el bot.

--- src/bot.py
+++ src/bot.py
@@ -5,6 +5,16 @@ from discord.ext import commands
 from dotenv import load_dotenv
 import signal
 import asyncio
+import logging
+
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s %(levelname)s \t %(name)s %(message)s',
+    style='%',
+    datefmt='%Y-%m-%d %H:%M:%S'
+)
+
+logger = logging.getLogger("BOT")
 
 load_dotenv()
 TOKEN = os.getenv("DISCORD_BOT_TOKEN")
@@ -16,7 +26,7 @@ intents.guilds = True
 bot = commands.Bot(command_prefix="!", intents=intents)
 
 def signal_handler(signal, frame):
-    print("\n[BOT] Recibida señal de terminación. Desconectando...")
+    logger.info("Recibida señal de terminación. Desconectando...")
     loop = asyncio.get_event_loop()
     loop.create_task(shutdown_bot())
 
@@ -25,7 +35,7 @@ signal.signal(signal.SIGINT, signal_handler)
 @bot.event
 async def on_ready():
     
-    print(f"[BOT] Conectado como {bot.user}")
+    logger.info(f"Conectado como {bot.user}")
     await bot.load_extension("cogs.commands")
 
     await registrar_comandos()
@@ -34,24 +44,24 @@ async def on_ready():
 async def shutdown_bot():
     await limpiar_comandos()
     await bot.tree.sync(guild=None)  # Sincronización de comandos globales
-    print("[BOT] Comandos globales actualizados correctamente.")
-    print("[BOT] Desconectando...")
+    logger.info("Comandos globales actualizados correctamente.")
+    logger.info("Desconectando...")
     await bot.close()
 
 async def limpiar_comandos():
     try:
-        print("[BOT] Limpiando comandos...")
+        logger.info("Limpiando comandos...")
         bot.tree.clear_commands(guild=None)
         await bot.tree.sync(guild=None)  # Limpieza de comandos
-        print("[BOT] Comandos limpiados correctamente.")
+        logger.info("Comandos limpiados correctamente.")
     except Exception as e:
-        print(f"[BOT] Error al limpiar comandos. Trace: {e}")
+        logger.error(f"Error al limpiar comandos. Trace: {e}")
 
 async def registrar_comandos():
     try:
-        print("[BOT] Registrando comandos...")
+        logger.info("Registrando comandos...")
         await bot.tree.sync()  # Registro de comandos
-        print(f"[BOT] Comandos registrados correctamente. {len(bot.tree.get_commands())} comandos registrados.")
+        logger.info(f"Comandos registrados correctamente. {len(bot.tree.get_commands())} comandos registrados.")
     except Exception as e:
-        print(f"[BOT] Error al registrar comandos. Trace: {e}")
+        logger.error(f"Error al registrar comandos. Trace: {e}")
 bot.run(TOKEN)
