9a3070dec5cd9784852edcf7db84bfa15eddf8b5
elterror6 <danibasketball2000@gmail.com>
2025-07-04 12:54:30 +0200

    add(bot): Documentation.

--- src/bot.py
+++ src/bot.py
@@ -1,3 +1,6 @@
+"""
+Este archivo es el punto de entrada del bot de Discord.
+"""
 import discord
 import os
 from dotenv import load_dotenv
@@ -27,6 +30,9 @@ intents.members = True
 bot = commands.Bot(command_prefix="!", intents=intents)
 
 def signal_handler(signal, frame):
+    """"
+    Maneja la señal de terminación del bot (Ctrl + C).
+    """
     logger.info("Recibida señal de terminación. Desconectando...")
     loop = asyncio.get_event_loop()
     loop.create_task(shutdown_bot())
@@ -35,7 +41,11 @@ signal.signal(signal.SIGINT, signal_handler)
 
 @bot.event
 async def on_ready():
-    
+    """
+    Evento que se ejecuta cuando el bot se conecta a Discord.
+    Registra los comandos y carga las extensiones.
+    También inicializa el archivo de saludos y registra los comandos globales.
+    """
     logger.info(f"Conectado como {bot.user}")
     logger.info(f"Cargando datos ...")
     init_salute_file(bot)
@@ -52,6 +62,9 @@ async def on_ready():
 
 
 async def shutdown_bot():
+    """
+    Cierra el bot de Discord y limpia los comandos.
+    """
     await limpiar_comandos()
     await bot.tree.sync(guild=None)  # Sincronización de comandos globales
     logger.info("Comandos globales actualizados correctamente.")
@@ -59,6 +72,10 @@ async def shutdown_bot():
     await bot.close()
 
 async def limpiar_comandos():
+    """
+    Limpia los comandos del bot.
+    Esto es útil para evitar conflictos con comandos antiguos.
+    """
     try:
         logger.info("Limpiando comandos...")
         bot.tree.clear_commands(guild=None)
@@ -68,6 +85,10 @@ async def limpiar_comandos():
         logger.error(f"Error al limpiar comandos. Trace: {e}")
 
 async def registrar_comandos():
+    """
+    Registra los comandos del bot.
+    Esto se hace al iniciar el bot y también al cargar las extensiones.
+    """
     try:
         logger.info("Registrando comandos...")
         await bot.tree.sync()  # Registro de comandos
@@ -76,15 +97,27 @@ async def registrar_comandos():
         logger.error(f"Error al registrar comandos. Trace: {e}")
 
 async def cargar_extensiones():
+    """
+    Carga las extensiones del bot.
+    Las extensiones son módulos que contienen comandos y eventos del bot.
+    """
     await bot.load_extension("cogs.commands")
     await bot.load_extension("cogs.salute")
 
 @bot.event
 async def on_guild_join(guild):
+    """
+    Evento que se ejecuta cuando el bot se une a un nuevo servidor.
+    Registra el servidor y crea un saludo predeterminado.
+    """
     logger.info(f"Unido al servidor: {guild.name} (ID: {guild.id})")
     add_guild_salute(guild.id)
 @bot.event
 async def on_guild_remove(guild):
+    """
+    Evento que se ejecuta cuando el bot es eliminado de un servidor.
+    Elimina los datos de saludo del servidor.
+    """
     logger.info(f"Desconectado del servidor: {guild.name} (ID: {guild.id})")
     data = load_salute_data()
     if str(guild.id) in data:
@@ -94,6 +127,10 @@ async def on_guild_remove(guild):
 
 @bot.event
 async def on_member_join(member):
+    """
+    Evento que se ejecuta cuando un nuevo miembro se une a un servidor.
+    Envía un mensaje de saludo al canal de texto predeterminado del servidor.
+    """
     data = load_salute_data()
     gui = str(member.guild.id)
 
